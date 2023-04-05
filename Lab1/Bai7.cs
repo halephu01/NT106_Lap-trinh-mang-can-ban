fda3a76d28aea75de1d2dccdc7301600486f680a
Phú Hà Lê <21522465@ms.uit.edu.vn>
2023-04-06 00:08:22 +0700

    update bai7 lab1

--- Lab1/Bai7.cs
+++ Lab1/Bai7.cs
@@ -24,6 +24,7 @@ namespace Lab1
             else
             {
                 int subnet, soMang, NetID, HostID;
+                bool check = true;
                 subnet = Int32.Parse(tbSubnet.Text);
                 soMang = Int32.Parse(tbSoMang.Text);
                 if (arr[0] > 191 && arr[0] < 224)
@@ -31,78 +32,84 @@ namespace Lab1
                     if (subnet < 24 || subnet > 32 || soMang > 2097158 || soMang < 0)
                     {
                         MessageBox.Show("Vui lòng nhập lại Subnet Mask hoặc số mạng!");
+                        check = false;
                     }
+
                 }
                 if (arr[0] > 127 && arr[0] < 192)
                 {
-                    if(subnet < 16 || subnet > 32|| soMang > 16384 || soMang < 0)
+                    if (subnet < 16 || subnet > 32 || soMang > 16384 || soMang < 0)
                     {
                         MessageBox.Show("Vui lòng nhập lại Subnet Mask hoặc số mạng!");
+                        check = false;
                     }
                 }
                 if (arr[0] > 0 && arr[0] < 127)
                 {
-                    if (subnet < 8 || subnet > 32 || soMang < 0 || soMang > 128) 
+                    if (subnet < 8 || subnet > 32 || soMang < 0 || soMang > 128)
                     {
                         MessageBox.Show("Vui lòng nhập lại Subnet Mask hoặc số mạng!");
+                        check = false;
                     }
                 }
-
-                int bitMuon = 0, STT = 1;
-                for (int i = 0; i < soMang; i++)
+                if (check)
                 {
-                    if (Math.Pow(2, i) >= soMang)
+                    int bitMuon = 0, STT = 1;
+                    for (int i = 0; i < soMang; i++)
                     {
-                        bitMuon = i;
-                        STT = (int)Math.Pow(2, i);
-                        break;
-                    }
-                }
-
-                int buocNhay;
-                NetID = subnet + bitMuon;
-                HostID = 32 - NetID;
-
-                for (int i = 1; i <= STT; i++)
-                {
-                    ListViewItem item = new ListViewItem(i.ToString());
-
-                    string diaChiMang = "", diaChiDau = "", diaChiCuoi = "", broadcast = "";
-
-                    if (arr[0] > 0 && arr[0] < 127)
-                    {
-                        buocNhay = (int)Math.Pow(2, 8 - bitMuon);
-                        broadcast = broadcast + arr[0] + "." + (int)(arr[1] + buocNhay - 1) + ".255.255";
-                        diaChiCuoi = diaChiCuoi + arr[0] + "." + (int)(arr[1] + buocNhay - 1) + ".255.254";
-                        diaChiDau = diaChiDau + arr[0] + "." + arr[1] + ".0.1";
-                        diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
-                        arr[1] = arr[1] + buocNhay;
-                    }
-                    else if (arr[0] > 126 && arr[0] < 192)
-                    {
-                        buocNhay = (int)Math.Pow(2, 8 - bitMuon);
-
-                        broadcast = broadcast + arr[0] + "." + arr[1] + "." + (int)(arr[2] + buocNhay - 1) + ".255";
-                        diaChiCuoi = diaChiCuoi + arr[0] + "." + arr[1] + "." + (int)(arr[2] + buocNhay - 1) + ".254";
-                        diaChiDau = diaChiDau + arr[0] + "." + arr[1] + "." + arr[2] + ".1";
-                        diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
-                        arr[2] = arr[2] + buocNhay;
-                    }
-                    else
-                    {
-                        buocNhay = (int)Math.Pow(2, 8 - bitMuon);
-                        diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
-                        diaChiDau = diaChiDau + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + 1);
-                        diaChiCuoi = diaChiCuoi + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + buocNhay - 2);
-                        broadcast = broadcast + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + buocNhay - 1);
-                        arr[3] = arr[3] + buocNhay;
+                        if (Math.Pow(2, i) >= soMang)
+                        {
+                            bitMuon = i;
+                            STT = (int)Math.Pow(2, i);
+                            break;
+                        }
                     }
 
-                    item.SubItems.Add(diaChiMang);
-                    item.SubItems.Add(diaChiDau);
-                    item.SubItems.Add(diaChiCuoi);
-                    item.SubItems.Add(broadcast);
-                    listView.Items.Add(item);
+                    int buocNhay;
+                    NetID = subnet + bitMuon;
+                    HostID = 32 - NetID;
+
+                    for (int i = 1; i <= STT; i++)
+                    {
+                        ListViewItem item = new ListViewItem(i.ToString());
+
+                        string diaChiMang = "", diaChiDau = "", diaChiCuoi = "", broadcast = "";
+
+                        if (arr[0] > 0 && arr[0] < 127)
+                        {
+                            buocNhay = (int)Math.Pow(2, 8 - bitMuon);
+                            broadcast = broadcast + arr[0] + "." + (int)(arr[1] + buocNhay - 1) + ".255.255";
+                            diaChiCuoi = diaChiCuoi + arr[0] + "." + (int)(arr[1] + buocNhay - 1) + ".255.254";
+                            diaChiDau = diaChiDau + arr[0] + "." + arr[1] + ".0.1";
+                            diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
+                            arr[1] = arr[1] + buocNhay;
+                        }
+                        else if (arr[0] > 126 && arr[0] < 192)
+                        {
+                            buocNhay = (int)Math.Pow(2, 8 - bitMuon);
+
+                            broadcast = broadcast + arr[0] + "." + arr[1] + "." + (int)(arr[2] + buocNhay - 1) + ".255";
+                            diaChiCuoi = diaChiCuoi + arr[0] + "." + arr[1] + "." + (int)(arr[2] + buocNhay - 1) + ".254";
+                            diaChiDau = diaChiDau + arr[0] + "." + arr[1] + "." + arr[2] + ".1";
+                            diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
+                            arr[2] = arr[2] + buocNhay;
+                        }
+                        else
+                        {
+                            buocNhay = (int)Math.Pow(2, 8 - bitMuon);
+                            diaChiMang = diaChiMang + arr[0] + "." + arr[1] + "." + arr[2] + "." + arr[3];
+                            diaChiDau = diaChiDau + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + 1);
+                            diaChiCuoi = diaChiCuoi + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + buocNhay - 2);
+                            broadcast = broadcast + arr[0] + "." + arr[1] + "." + arr[2] + "." + (int)(arr[3] + buocNhay - 1);
+                            arr[3] = arr[3] + buocNhay;
+                        }
+
+                        item.SubItems.Add(diaChiMang);
+                        item.SubItems.Add(diaChiDau);
+                        item.SubItems.Add(diaChiCuoi);
+                        item.SubItems.Add(broadcast);
+                        listView.Items.Add(item);
+                    }
                 }
             }
         }
